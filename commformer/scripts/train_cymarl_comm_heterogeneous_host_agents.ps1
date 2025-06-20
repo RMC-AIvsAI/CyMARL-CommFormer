43919bf4ce1e6978d6f74afcc73008dde5abe37c
alexwtp <alexwtp@hotmail.com>
2025-06-19 21:51:19 -0400

    New host agent scenario and script.
    Added a new scenario file for the CyMARL CommFormer experiment with heterogeneous host agents, and a PowerShell script to train the model.
    Also updated the Jupyter notebook for experiment analysis to include this new scenario as well as data from multiple runs using the previous scenario.

---
+++ commformer/scripts/train_cymarl_comm_heterogeneous_host_agents.ps1
@@ -0,0 +1,4 @@
+# Trains 5 x CommFormer heterogeneous agents in an identical network topology to that seen in CyMARL phase 2.
+# Refer to the scenario file for specifics on agent actions and network topology.
+python .\train\train_cymarl_comm.py --env_name "CybORG" --algorithm_name "commformer_dec" --scenario_name "phase2_confidentiality_small_heterogeneous_host_agents"`
+--eval_episode_length 30 --action_limiting "True" --n_rollout_threads 48 --episode_length 30 --num_env_steps 20000000 --log_interval 10 --use_bilevel "True" --seed 1 --alg_seed 1 --use_eval "True" --eval_interval 10 --n_eval_rollout_threads 2 --eval_episodes 2 --sparsity 0.7 --use_linear_lr_decay "True"
